6ca14e1e4ce4de8153205b730ea4897efcb80345
Lary Ace Pantonial <laryacepantonial017@gmail.com>
2025-05-14 19:26:22 +0800

    Add files via upload

---
+++ Portfolio_Pantonial/script.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    }
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multiple-text' ,{
+    strings: ['Frontend Developer', 'Video Editor'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+})
